b7a4d874b258d6910da3351792bda7298a1c8896
BhavyaMPatel <bhavyapatel4012@gmail.com>
2024-09-02 16:29:18 +0530

    Adding RandomQueries File

---
+++ RandomQuery.js
@@ -0,0 +1,9 @@
+const RandomQuery = [
+    "What is The Time At New York",
+    "Best University In The World",
+    "Ola Share Price",
+    "What Is The Market Condition",
+    "Add More Query Here !!!"
+]
+
+export default RandomQuery;
